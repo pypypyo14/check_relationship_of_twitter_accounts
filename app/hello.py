54166eed2b0cd9001efddf8eaddb6ae0c560da9d
Saori <jkmr9214@gmail.com>
2019-06-29 19:13:33 +0900

    Bootstrap Setting

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "index page"
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
