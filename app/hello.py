ea9335477923b3543933356a75e6a859ebd571ae
Saori <jkmr9214@gmail.com>
2019-06-29 22:37:36 +0900

    validation通ったらとりあえずresultが出てくるように変更(TwitterAPIアクセス処理は未実装)

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,4 @@
-import os, json, config
+import os
 from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -15,9 +15,22 @@ class TwitterUserAccount(FlaskForm):
     user_id_2 = StringField('ID', validators=[DataRequired()], render_kw={"placeholder": "username"})
     submit = SubmitField('Submit')
 
-class TwitterAccount:
-    def isFollow(self, followee):
+def FollowingCheck(user_id_1, user_id_2):
+    followflags={'User1followUser2':'0', 'User2followUser1':'0'}
+    if isFollow(user_id_1, user_id_2):
+        followflags["User1followUser2"] = 1
+    if isFollow(user_id_2, user_id_1):
+        followflags["User2followUser1"] = 1
+    return followflags
+
+def isFollow(follower, followee):
         pass
+
+def FollowingCheckInJapanese(followflag):
+    print(followflag)
+    if followflag == 1:
+        return "います"
+    return "いません"
 
 def isSameTwitterAccount(user_id_1, user_id_2):
     if user_id_1 == user_id_2:
@@ -32,9 +45,13 @@ def index():
         user_id_1 = request.form['user_id_1']
         user_id_2 = request.form['user_id_2']
         if isSameTwitterAccount(user_id_1, user_id_2):
-            print(True)
             flash(u'異なるユーザー同士を入力してください', 'alert alert-danger')
-        return render_template('index.html', form = form)
+            return render_template('index.html', form = form)
+        else:
+            followflags = FollowingCheck(user_id_1, user_id_2)
+            message1 = "@" + user_id_1 + " は @" + user_id_2 + " をフォローして" + FollowingCheckInJapanese(followflags['User1followUser2'])
+            message2 = "@" + user_id_2 +  "は @" + user_id_1 + " をフォローして" + FollowingCheckInJapanese(followflags['User2followUser1'])
+            return render_template('index.html', form = form, results = [message1, message2])
     else:
         return render_template('index.html', form = form)
 
